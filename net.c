c88090a9ceefd9d8fb2cc6f22cb06c3f0a8c16cc
tesso57 <tesso1729@gmail.com>
2022-03-03 10:34:05 +0000

    step4

--- net.c
+++ net.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "platform.h"
 
 #include "util.h"
 #include "net.h"
+#include "ip.h"
+
+struct net_protocol
+{
+    struct net_protocol *next;
+    uint16_t type;
+    struct queue_head queue;
+    void (*handler)(const uint8_t *data, size_t len, struct net_device *dev);
+};
+
+struct net_protocol_queue_entry
+{
+    struct net_device *dev;
+    size_t len;
+    uint8_t data[];
+};
 
 static struct net_device *devices;
+static struct net_protocol *protocols;
 
 struct net_device *net_device_alloc(void)
 {
@@ -102,10 +120,60 @@ int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data
     return 0;
 }
 
+int net_protocol_register(uint16_t type, void (*handler)(const uint8_t *data, size_t len, struct net_device *dev))
+{
+    struct net_protocol *proto;
+
+    for (proto = protocols; proto; proto = proto->next)
+    {
+        if (type == proto->type)
+        {
+            errorf("already registered, type=0x%04x", type);
+            return -1;
+        }
+    }
+    proto = memory_alloc(sizeof(*proto));
+    if (!proto)
+    {
+        errorf("memory_alloc() failure");
+        return -1;
+    }
+    proto->type = type;
+    proto->handler = handler;
+    proto->next = protocols;
+    infof("registered, type=0x%04x", type);
+    return 0;
+}
+
 int net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
-    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
-    debugdump(data, len);
+    struct net_protocol *proto;
+    struct net_protocol_queue_entry *entry;
+
+    for (proto = protocols; proto; proto = proto->next)
+    {
+        if (proto->type == type)
+        {
+            entry = memory_alloc(sizeof(*entry) + len);
+            if (!entry)
+            {
+                errorf("memory_alloc() failure");
+                return -1;
+            }
+            entry->dev = dev;
+            entry->len = len;
+            memcpy(entry->data, data, len);
+            if (!queue_push(&proto->queue, entry))
+            {
+                errorf("queue_push() failure");
+                memory_free(entry);
+                return -1;
+            }
+            debugf("queue pushed (num:%u), dev=%s, type=0x%04x, len=%zu", proto->queue.num, dev->name, type, len);
+            debugdump(data, len);
+            return 0;
+        }
+    }
     return 0;
 }
 
@@ -146,6 +214,12 @@ int net_init(void)
     if (intr_init() == -1)
     {
         errorf("intr_init() failure");
+        return -1;
+    }
+
+    if (ip_init() == -1)
+    {
+        errorf("ip_init() failure");
         return -1;
     }
     infof("initialized");
